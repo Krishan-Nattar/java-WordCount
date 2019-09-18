54a4bc7f3218457e69a040feaaf0219352fa24ae
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-17 18:21:29 -0700

    'commit'

---
+++ src/wordcount/Words.java
@@ -0,0 +1,26 @@
+package wordcount;
+
+public class Words
+{
+    private int count =0;
+    private String word;
+
+    public Words(String word)
+    {
+        this.word = word;
+        this.count += 1;
+    }
+    
+
+    public int getCount()
+    {
+        return this.count;
+    }
+
+    public void addCount()
+    {
+        this.count += 1;
+    }
+
+    
+}
